7df9af31077d015a8a2225be85445b4d7db77c51
amos <amos.weiskopf+attack@gmail.com>
2024-01-18 21:39:41 +0200

    fix to install.sh (changed 'source' to '.')

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 curl -o table2view.py https://raw.githubusercontent.com/amosWeiskopf/table2view/main/table2view.py
 chmod +x table2view.py
 echo 'alias table2view="python3 table2view.py"' >> ~/.bashrc
-source ~/.bashrc
+. ~/.bashrc
 rm table2view.py
