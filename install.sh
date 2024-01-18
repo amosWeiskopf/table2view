d97ab07606aa142a9bfa28dcc6e8d459699c9cf9
amos <amos.weiskopf+attack@gmail.com>
2024-01-18 21:31:23 +0200

    Update to install.sh: fixed local save to current working dir

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-curl -o ~/scripts/table2view.py https://raw.githubusercontent.com/amosWeiskopf/table2view/main/table2view.py
-chmod +x ~/scripts/table2view.py
-echo 'alias table2view="python3 ~/scripts/table2view.py"' >> ~/.bashrc
+curl -o table2view.py https://raw.githubusercontent.com/amosWeiskopf/table2view/main/table2view.py
+chmod +x table2view.py
+echo 'alias table2view="python3 table2view.py"' >> ~/.bashrc
 source ~/.bashrc
+rm table2view.py
